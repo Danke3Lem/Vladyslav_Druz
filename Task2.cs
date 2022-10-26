3850b665184542e0b9e721e60d3d85301c1ddff6
Danke3Lem <vladyslavdruz@gmail.com>
2022-10-26 13:16:36 +0300

    Task #2 and Tests

--- Task2.cs
+++ Task2.cs
@@ -1,51 +1,101 @@
 ﻿using System;
 
-public class Task2
+namespace Task2
 {
-    public static char First_Non_Repeating_Letter(string inputString)
-    {   
-        //Dictionary was selected to count the amount of specific char
-
-        Dictionary<char, int> allLettersFromTheString = new Dictionary<char, int>();
-        string originalInputString = inputString;
-
-        //String is lower to count specific letter, where upper case is included as well
-        inputString = inputString.ToLower();
-        int tmp = 0;
-
-        foreach (char ch in inputString.ToCharArray())
+    public class Task2
+    {
+        public static char First_Non_Repeating_Letter(string inputString)
         {
-            if (allLettersFromTheString.ContainsKey(ch))
-            {
-                tmp = allLettersFromTheString[ch];
-                allLettersFromTheString[ch] = tmp + 1;
+            //Dictionary was selected to count the amount of specific char
 
-                continue;
+            Dictionary<char, int> allLettersFromTheString = new Dictionary<char, int>();
+            string originalInputString = inputString;
+
+            //String is lower to count specific letter, where upper case is included as well
+            inputString = inputString.ToLower();
+            int tmp = 0;
+
+            foreach (char ch in inputString.ToCharArray())
+            {
+                if (allLettersFromTheString.ContainsKey(ch))
+                {
+                    tmp = allLettersFromTheString[ch];
+                    allLettersFromTheString[ch] = tmp + 1;
+
+                    continue;
+                }
+
+
+                allLettersFromTheString.Add(ch, 1);
             }
 
+            //Find the first char that has value == 1 (was repeated once)
 
-            allLettersFromTheString.Add(ch, 1);
-        }
-
-        //Find the first char that has value == 1 (was repeated once)
-
-        if (allLettersFromTheString.Values.Contains(1))
-        {
-            char resultCh = allLettersFromTheString.First(x => x.Value == 1).Key;
-            var index = inputString.IndexOf(resultCh);
-            bool IsLetterUpper = Char.IsUpper(originalInputString, index);
-
-            if (IsLetterUpper)
+            if (allLettersFromTheString.Values.Contains(1))
             {
-                return Char.ToUpper(resultCh);
+                char resultCh = allLettersFromTheString.First(x => x.Value == 1).Key;
+                var index = inputString.IndexOf(resultCh);
+                bool IsLetterUpper = Char.IsUpper(originalInputString, index);
+
+                if (IsLetterUpper)
+                {
+                    return Char.ToUpper(resultCh);
+                }
+
+                return Char.ToLower(resultCh);
             }
 
-            return Char.ToLower(resultCh);
+            else
+            {
+                return ' '; //Empty char
+            }
+        }
+    }
+
+    public class Tests2 // Task 1.2
+    {
+        [Test]  // Test #1 for Task 1.2
+        public static void Test3()
+        {
+            //Arrange
+            string inputString = "stress";
+            char expected = 't';
+
+            //Act
+            char actual = Task2.First_Non_Repeating_Letter(inputString);
+
+            //Assert
+            Assert.AreEqual(expected, actual);
         }
 
-        else
+        [Test]  // Test #2 for Task 1.2
+        public static void Test4()
         {
-            return ' '; //Empty char
+            //Arrange
+            string inputString = "sTreSS";
+            char expected = 'T';
+
+            //Act
+            char actual = Task2.First_Non_Repeating_Letter(inputString);
+
+            //Assert
+            Assert.AreEqual(expected, actual);
         }
+
+        [Test]  // Test #3 for Task 1.2
+        public static void Test5()
+        {
+            //Arrange
+            string inputString = "ShLlahSa"; // The string, where all letters are repeated twice
+            char expected = ' ';
+
+            //Act
+            char actual = Task2.First_Non_Repeating_Letter(inputString);
+
+            //Assert
+            Assert.AreEqual(expected, actual);
+        }
+
+
     }
 }
