48f87fff368ce88877aaebf76fe4a3797421ef21
Danke3Lem <vladyslavdruz@gmail.com>
2022-10-26 13:17:10 +0300

    Task #4 and Tests

--- Task4.cs
+++ Task4.cs
@@ -1,22 +1,58 @@
 ﻿using System;
 
-public class Task4
+namespace Task4
 {
-	public static int AmountOfPairsForTargetNumber(int[] inputArr, int targetNum)
-	{
-		int result = 0;
+    public class Task4
+    {
+        public static int AmountOfPairsForTargetNumber(int[] inputArr, int targetNum)
+        {
+            int result = 0;
 
-		for(int i = 0; i < inputArr.Length; i++)
-		{
-			for(int j = i + 1; j <inputArr.Length; j++)
-			{
-				if(inputArr[i] + inputArr[j] == targetNum)
-				{
-					result++;
-				}
-			}
-		}
+            for (int i = 0; i < inputArr.Length; i++)
+            {
+                for (int j = i + 1; j < inputArr.Length; j++)
+                {
+                    if (inputArr[i] + inputArr[j] == targetNum)
+                    {
+                        result++;
+                    }
+                }
+            }
 
-		return result;
-	}
+            return result;
+        }
+    }
+
+    public class Tests4 // Task 1.4
+    {
+        [Test]  // Test #1 for Task 1.4
+        public static void Test8()
+        {
+            //Arrange
+            int[] inputArr = new int[] { 1, 3, 6, 2, 2, 0, 4, 5 };
+            int targetNum = 5;
+            int expected = 4;
+
+            //Act
+            int actual = Task4.AmountOfPairsForTargetNumber(inputArr, targetNum);
+
+            //Assert
+            Assert.AreEqual(expected, actual);
+        }
+
+        [Test]  // Test #2 for Task 1.3
+        public static void Test9()
+        {
+            // Arrange
+            int[] inputArr = new int[] { 5, 1, 4, 6, 3, 0, 2, 1 };
+            int targetNum = 6;
+            int expected = 4;
+
+            //Act
+            int actual = Task4.AmountOfPairsForTargetNumber(inputArr, targetNum);
+
+            //Assert
+            Assert.AreEqual(expected, actual);
+        }
+    }
 }
