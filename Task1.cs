0992f1e971f6fe6a725d9570af77b901ca4d5d73
Danke3Lem <vladyslavdruz@gmail.com>
2022-10-26 13:16:17 +0300

    Task #1 and Tests

--- Task1.cs
+++ Task1.cs
@@ -1,12 +1,60 @@
 ﻿using System;
 
-public class Task1
+namespace Task1
 {
-    public static IEnumerable<int> GetIntegersFromList(List<object> elements)
+    public class Task1
     {
-        //Adding only int-type objects
+        public static IEnumerable<int> GetIntegersFromList(List<object> elements)
+        {
+            //Adding only int-type objects
 
-        List<int> result = elements.OfType<int>().ToList();
-        return result;
+            List<int> result = elements.OfType<int>().ToList();
+            return result;
+        }
+    }
+
+    public class Tests1 // Task 1.1
+    {
+        [Test]  // Test #1 for Task 1.1
+        public static void Test1()
+        {
+            //Arrange
+            List<object> list = new List<object>();
+            list.Add(1);
+            list.Add(2);
+            list.Add("bbb");
+
+            List<object> expected = new List<object>();
+            expected.Add(1);
+            expected.Add(2);
+
+            //Act
+            List<int> actual = new List<int>(Task1.GetIntegersFromList(list));
+
+            //Assert
+            CollectionAssert.AreEqual(expected, actual);
+        }
+
+        [Test] // Test #2 for Task 1.1
+        public static void Test2()
+        {
+            //Arrange
+            List<object> list = new List<object>();
+            list.Add("15");
+            list.Add(5);
+            list.Add("5bb");
+            list.Add("0");
+            list.Add(9);
+
+            List<object> expected = new List<object>();
+            expected.Add(5);
+            expected.Add(9);
+
+            //Act
+            List<int> actual = new List<int>(Task1.GetIntegersFromList(list));
+
+            //Assert
+            CollectionAssert.AreEqual(expected, actual);
+        }
     }
 }
